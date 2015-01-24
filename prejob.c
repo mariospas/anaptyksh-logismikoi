4611e94c5da8c80e2913d0abdbbbae87081f2e7f
marios <mariospas@gmail.com>
2015-01-24 18:43:26 +0200

    mutex

--- prejob.c
+++ prejob.c
@@ -1,5 +1,24 @@
 #include "prejob.h"
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <dirent.h>
 
+struct findTopN
+{
+	ptr_array_matches array;
+	ptr_entry data;
+};
 
 ptr_array_matches find_topN_forums(ptr_graph forum_graph,int N)
 {
@@ -7,31 +26,64 @@ ptr_array_matches find_topN_forums(ptr_graph forum_graph,int N)
 	HT_iter_ptr iter;
 	ht_ptr nodes = Graph_nodes(forum_graph);
 	ptr_entry data;
+	ptr_findTopN topNdata;
 
 	ptr_array_matches array = create_array_match(N);
-	ptr_matches match;
-	double similarity = 0.0;
 
 	iter = HT_iter_create(nodes);
 	//printf("In matchSuggestion\n");
-	list_ptr member_list;
-	double members;
+
+	pthread_t* thread_w = (pthread_t *)malloc(graph_size * sizeof(pthread_t));
+	int err;
+
 
 	for(i=0;i<graph_size;i++)
 	{
 		data = ((ptr_entry)HT_iter_data(iter));
-		member_list = type_list(data,"forum_hasMember_person.csv");
+		topNdata = malloc(sizeof(struct findTopN));
+		topNdata->array = array;
+		topNdata->data = data;
 
-		if(member_list == NULL) continue;
-
-		members = LL_size(member_list);
-		match = create_match(data->id,members);
-		insert_match(array,match);
-
+		err = pthread_create(&thread_w[i],NULL,array_thread_insert,((void *) topNdata)) ;
+		if(err)
+		{
+			perror2("pthread_create_find_TopN_forums",err);
+		}
 
 		HT_iter_next(iter);
 	}
 	HT_iter_destroy(iter);
 
+	for(i=0;i<graph_size;i++)
+	{
+		pthread_join(thread_w[i],NULL);
+	}
+
 	return array;
+}
+
+
+void* array_thread_insert(void* topNdata)
+{
+	ptr_array_matches array = ((ptr_findTopN) topNdata)->array;
+	ptr_entry data = ((ptr_findTopN) topNdata)->data;
+	int ret = 22;
+	list_ptr member_list;
+	double members;
+	ptr_matches match;
+
+	member_list = type_list(data,"forum_hasMember_person.csv");
+
+	if(member_list != NULL)
+	{
+		members = LL_size(member_list);
+		match = create_match(data->id,members);
+
+		pthread_mutex_lock(&mutex_find_top_n);
+		insert_match(array,match);
+		pthread_mutex_unlock(&mutex_find_top_n);
+	}
+
+	free(topNdata);
+	pthread_exit(&ret);
 }
--- prejob.h
+++ prejob.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "queries.h"
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
@@ -15,11 +14,20 @@
 #include <sys/types.h>
 #include <pthread.h>
 #include <dirent.h>
+#include "queries.h"
 #include "database.h"
 #include "graph.h"
 #include "graph_stats.h"
 #include "graph_entry.h"
 
+#define perror2(s, e) fprintf(stderr, "%s: %s\n",s,strerror(e))
+
+typedef struct findTopN *ptr_findTopN;
+
+
+pthread_mutex_t mutex_find_top_n = PTHREAD_MUTEX_INITIALIZER;
 
 
 ptr_array_matches find_topN_forums(ptr_graph forum_graph,int N);
+
+void* array_thread_insert(void* topNdata);
